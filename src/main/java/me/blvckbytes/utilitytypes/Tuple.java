bfa840d77b52b76d94f320029fb60eb2b235662d
Philip Hell <blvckbytes@gmail.com>
2023-03-04 19:46:21 +0100

    got rid of lombok

--- src/main/java/me/blvckbytes/utilitytypes/Tuple.java
+++ src/main/java/me/blvckbytes/utilitytypes/Tuple.java
@@ -24,10 +24,12 @@
 
 package me.blvckbytes.utilitytypes;
 
-import lombok.AllArgsConstructor;
-
-@AllArgsConstructor
 public class Tuple<A, B> {
   public final A a;
   public final B b;
+
+  public Tuple(A a, B b) {
+    this.a = a;
+    this.b = b;
+  }
 }
